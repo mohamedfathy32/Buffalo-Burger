e334f74cff972aeba845b29c0000375699614854
mohamedfathy32 <mf876026@gmail.com>
2024-09-30 20:10:42 +0300

    instal packge and tailwin

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs bg-red-700">
         Click on the Vite and React logos to learn more
       </p>
     </>
