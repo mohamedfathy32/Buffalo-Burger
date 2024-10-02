e7b8d4ef7483008b8bea708bd04c38812cdcd76b
mostafadardeir1 <mostafadardeir1@gmail.com>
2024-10-03 00:35:30 +0300

    Update Splash.jsx

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -23,13 +23,13 @@ function Splash() {
                 className="w-[250px] h-[250px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
             >
                 <img
-                    src="/public/IconBg.png"
+                    src="/images/splash/IconBg.png"
                     alt="IconBg"
                     id="IconBg"
                     className="w-full h-full"
                 />
                 <img
-                    src="/public/IconNoBg.png"
+                    src="/images/splash/IconNoBg.png"
                     alt="IconNoBg"
                     id="IconNoBg"
                     className="absolute top-[30%] left-[25%] w-1/2 h-1/2"
