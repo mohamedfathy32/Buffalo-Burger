b6479d8c1d40dd2567056849e7c40d6f814704f9
Mostafa Dardeir <mostafadardeir1@gmail.com>
2024-10-03 22:13:21 +0300

    Update MealDetails.jsx

--- src/pages/Menu/MealDetails.jsx
+++ src/pages/Menu/MealDetails.jsx
@@ -5,11 +5,11 @@ export default function MealDetails() {
     const dish = location.state?.dish
     return (
         <>
-            <div className="flex flex-col md:flex-row bg-stone-900 items-center justify-center p-4">
+            <div className="bg-stone-900 flex justify-center items-center lg:flex-row flex-col p-4">
                 <img src={dish.image} alt={dish.title} className=" place-items-center w-36" />
-                <div>
-                    <h2 className="text-orange-500 text-2xl capitalize">{dish.title}</h2>
-                    <p className="text-white">{dish.description}</p>
+                <div className="lg:text-start text-center md:my-6 md:mx-16 ">
+                    <h2 className="text-orange-500 font-bold text-2xl capitalize">{dish.title}</h2>
+                    <p className="text-white max-w-[500px]">{dish.description}</p>
                 </div>
             </div>
         </>
