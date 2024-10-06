b29feb4c4b3584a93ea64642e196882c1b562016
Mostafa Dardeir <mostafadardeir1@gmail.com>
2024-10-06 13:01:14 +0300

    product list

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -6,7 +6,7 @@ export default function DishCard({ dish }) {
     return (
         <div className="h-96 max-w-full w-72 md:w-80 hover:-translate-y-2 duration-1000 cursor-pointer ease-in-out">
             <div className="relative h-44">
-                <img src={dish?.image} alt={dish?.title} className="w-44 h-44 absolute top-10 start-0" />
+                <img src={dish?.imageURL} alt={dish?.title} className="w-44 h-44 absolute top-10 start-0" />
                 <div className="w-44 h-44 absolute top-10 start-0"></div>
             </div>
             <div className="bg-neutral-50 rounded-xl p-4 pb-2 pt-14 flex flex-col h-52">
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import MenuNav from "../../components/MenuNav"
 import KeenSlider from "../../components/KeenSlider"
 import MenuCategory from "./MenuCategory"
+import { productsList } from "../../utils/ProdList"
 
 export const cat_burger = [
     {
@@ -116,7 +117,7 @@ export default function Menu() {
             <div className="sticky top-28 z-10 w-full" ><MenuNav /></div>
             <h1 id="offer" className="PSS text-7xl uppercase text-orange-600 text-center m-6">offers</h1>
             <div className="mx-3 my-6"><KeenSlider /></div>
-            <MenuCategory hash={'burger'} catList={cat_burger} catTitle={'Burger Sandwiches'} />
+            <MenuCategory hash={'burger'} catList={productsList} catTitle={'Burger Sandwiches'} />
         </>
     )
 }
