01c10f620724aa4ffab3047c3e026cbd80bee389
Mostafa Dardeir <mostafadardeir1@gmail.com>
2024-10-06 14:33:19 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}", 
+  "./src/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
   extend: {
