bdf961bfd9de01adc2f8f4e8af8bf67413766d8c
Mostafa Dardeir <mostafadardeir1@gmail.com>
2024-10-06 14:21:49 +0300

    Update AllProduct.js

--- src/utils/AllProduct.js
+++ src/utils/AllProduct.js
@@ -1,30 +1,20 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { collection, getDocs, query } from "firebase/firestore";
-import { useEffect, useState } from "react";
 import { db } from "./firebase";
-
-export const allProducts = ()=>{
-
-
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const q = query(collection(db, "product"));
-                const querySnapshot = await getDocs(q);
-                const productsArray = querySnapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                }));
-                setProducts(productsArray);
-            } catch (error) {
-                console.error("Error fetching products: ", error);
-            } 
-        };
-
-        fetchData();
-    }, []);
-
+export const allProducts = () => {
+    let products = [];
+    const fetchData = async () => {
+        try {
+            const q = query(collection(db, "product"));
+            const querySnapshot = await getDocs(q);
+            const productsArray = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            products = productsArray;
+        } catch (error) {
+            console.error("Error fetching products: ", error);
+        }
+    };
+    fetchData();
     return products;
 }
