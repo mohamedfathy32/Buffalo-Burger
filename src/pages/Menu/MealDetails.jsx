8a46f0ef1762d68ceaf53e7c3e6104c752ca0316
Mostafa Dardeir <mostafadardeir1@gmail.com>
2024-10-06 13:29:38 +0300

    installing mui

--- src/pages/Menu/MealDetails.jsx
+++ src/pages/Menu/MealDetails.jsx
@@ -6,12 +6,26 @@ export default function MealDetails() {
     return (
         <>
             <div className="bg-stone-900 flex justify-center items-center lg:flex-row flex-col p-4">
-                <img src={dish.image} alt={dish.title} className=" place-items-center w-[150px]" />
+                <img src={dish.imageURL} alt={dish.title} className=" place-items-center w-[150px] h-[150px]" />
                 <div className="lg:text-start text-center md:my md:mx-16 ">
                     <h2 className="text-orange-600 font-bold text-2xl capitalize">{dish.title}</h2>
                     <p className="text-white max-w-[500px]">{dish.description}</p>
                 </div>
             </div>
+            <div>
+                <h1>size</h1>
+                <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="female"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        <FormControlLabel value="other" control={<Radio />} label="Other" />
+                    </RadioGroup>
+                </FormControl>            </div>
         </>
     )
 }
