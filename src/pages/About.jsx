24c019552c64efddbbc9bab4f8aebb029c92b72f
Mohamed Ayman <monstertk10@gmail.com>
2024-10-14 01:59:44 +0300

    some update on about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,19 +15,19 @@ export default function AboutPage() {
     return (
         <>
             <div className="bg-orange-500 p-4 d-inline-block flex justify-center text-white">
-                <div className={`nav-item d-inline-block mx-2 ${index !== 0 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(0) }}>
+                <div className={`nav-item d-inline-block mx-2 cursor-pointer ${index !== 0 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(0) }}>
                     OUR STORY
                 </div>
-                <div className={`nav-item d-inline-block mx-2 ${index !== 1 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(1) }}>
+                <div className={`nav-item d-inline-block mx-2 cursor-pointer ${index !== 1 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(1) }}>
                     OUR VALUES
                 </div>
-                <div className={`nav-item d-inline-block mx-2 ${index !== 2 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(2) }}>
+                <div className={`nav-item d-inline-block mx-2 cursor-pointer ${index !== 2 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(2) }}>
                     QUALITY CONTROL
                 </div>
-                <div className={`nav-item d-inline-block mx-2 ${index !== 3 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(3) }}>
+                <div className={`nav-item d-inline-block mx-2 cursor-pointer ${index !== 3 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(3) }}>
                     CAREERS
                 </div>
-                <div className={`nav-item d-inline-block mx-2 ${index !== 4 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(4) }}>
+                <div className={`nav-item d-inline-block mx-2 cursor-pointer ${index !== 4 ? 'opacity-50' : ''}`} onClick={() => { changeIndex(4) }}>
                     FIND US
                 </div>
             </div>
