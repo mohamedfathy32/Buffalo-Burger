4ce3ea08d23ce5f9c33f49406d518b7b68db89c2
mohamedfathy32 <mf876026@gmail.com>
2024-10-16 16:21:27 +0300

    done profile ✔️

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,60 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { logedContext } from "../isLoged";
 import { useNavigate } from "react-router-dom";
+import { getUserInfoById } from "../utils/firebase";
 
 export default function ProfilePage() {
+    const [username, setUsername] = useState('');
+    const [useremail, setUseremail] = useState('');
+    const [formData, setFormData] = useState({
+        fullName: username,
+        phoneNumber: "01094976357",
+        email: useremail,
+    });
+    const [isChanged, setIsChanged] = useState(false);
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+        // Check if the form data has been changed
+        setIsChanged(
+            value !== (name === "fullName" ? username : name === "phoneNumber" ? "01094976357" : useremail)
+        );
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Perform the save operation (you can implement your save logic here)
+        console.log("Saving changes...", formData);
+
+        // After saving, reset isChanged to false
+        setIsChanged(false);
+    };
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            const userId = localStorage.getItem("userId");
+            if (userId) {
+                const userInfo = await getUserInfoById(userId);
+                if (userInfo) {
+                    setUsername(userInfo.username);
+                    setUseremail(userInfo.email);
+                    setFormData({
+                        fullName: userInfo.username,
+                        phoneNumber: userInfo.phoneNumber || "01094976357",
+                        email: userInfo.email,
+                    });
+                }
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
 
     const { isLoggedIn } = useContext(logedContext);
     const navigate = useNavigate();
@@ -15,8 +66,90 @@ export default function ProfilePage() {
     }, [isLoggedIn, navigate]);
 
     return (
-        <div>
-            profilePage
-        </div>
-    )
+        <>
+            <div className="w-full flex justify-center px-2 md:px-10 pt-7 pb-2">
+                <div className="w-full flex flex-col md:w-3/5">
+                    <div className="flex bg-black rounded-t-md p-3">
+                        <h1 className="PSS text-white tracking-wider uppercase text-5xl">Account</h1>
+                    </div>
+                    <div className="w-full flex flex-col p-1 md:p-4">
+                        <div className="flex w-full flex-col md:flex-row">
+                            <div className="flex flex-row md:justify-start justify-center items-start px-2 gap-x-4 md:flex-col border-[1px] border-[#f2f2f2] bg-transparent text-center w-full md:w-[30%] pt-5">
+                                <p className=" text-base pl-2 cursor-pointer font-bold text-[#1c1c1b] uppercase border-[#ff5f00] border-b-2 md:border-l-2 md:border-b-0">
+                                    Profile details
+                                </p>
+                                <p className=" text-base pl-2 cursor-pointer font-bold text-[#1c1c1b] uppercase">
+                                    Address book
+                                </p>
+                                <p className=" text-base pl-2 cursor-pointer font-bold text-[#1c1c1b] uppercase">
+                                    Change password
+                                </p>
+                            </div>
+                            <section className="w-full bg-transparent h-full">
+                                <div className="flex flex-col h-full mx-auto py-8 px-8">
+                                    <p className=" text-2xl hidden md:block font-bold text-[#1c1c1b]">
+                                        Profile details
+                                    </p>
+                                    <div className="flex flex-col gap-4 w-full">
+                                        <form onSubmit={handleSubmit}>
+                                            <div className="flex flex-col">
+                                                <p className="text-base pb-2 text-[#000]">Full name</p>
+                                                <div className="flex flex-col">
+                                                    <input
+                                                        type="text"
+                                                        name="fullName"
+                                                        value={formData.fullName}
+                                                        onChange={handleInputChange}
+                                                        autoComplete="off"
+                                                        required
+                                                        className="bg-white focus:border-transparent focus:bg-[#ffefe6] disabled:cursor-not-allowed disabled:bg-[#CCCCCC] disabled:text-black rounded-[10px] py-3 px-2 w-full ring-0"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <p className="text-base pb-2 text-[#000]">Phone number</p>
+                                                <div className="flex flex-col">
+                                                    <input
+                                                        type="text"
+                                                        name="phoneNumber"
+                                                        value={formData.phoneNumber}
+                                                        autoComplete="off"
+                                                        required
+                                                        className="bg-white focus:border-transparent focus:bg-[#ffefe6] disabled:cursor-not-allowed disabled:bg-[#CCCCCC] disabled:text-black rounded-[10px] py-3 px-2 w-full ring-0"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <p className="text-base pb-2 text-[#000]">E-mail</p>
+                                                <div className="flex flex-col">
+                                                    <input
+                                                        type="email"
+                                                        name="email"
+                                                        value={formData.email}
+                                                        onChange={handleInputChange}
+                                                        autoComplete="off"
+                                                        required
+                                                        className="bg-white focus:border-transparent focus:bg-[#ffefe6] disabled:cursor-not-allowed disabled:bg-[#CCCCCC] disabled:text-black rounded-[10px] py-3 px-2 w-full ring-0"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="flex mt-4">
+                                                <button
+                                                    type="submit"
+                                                    disabled={!isChanged} // Enable button only if the data has been changed
+                                                    className="bg-[#ff5f00] text-white h-9 px-4 font-main text-lg rounded-[10px] disabled:bg-[#ffcfb3]"
+                                                >
+                                                    Save changes
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -57,12 +57,34 @@ export async function getUsernameById(userId) {
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    return docSnap.data().username; // استرجاع اسم المستخدم
+    return docSnap.data().username; 
   } else {
     console.log("No such document!");
     return null;
   }
 }
+
+////////////////////////
+//function to get user informations by userID
+export async function getUserInfoById(userId) {
+  const docRef = doc(db, "users", userId);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    return {
+      username: data.username,
+      email: data.email
+    };
+  } else {
+    console.log("No such document!");
+    return null;
+  }
+}
+
+
+
+
+
 
 //////
 //auth
